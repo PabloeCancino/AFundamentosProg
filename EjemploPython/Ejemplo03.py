b3840cb88431e25808f6eae594a5e5bc8ba7c20a
davidmp <mamanipari@gmail.com>
2019-08-22 11:11:11 -0500

    Ejemplo 4

---
+++ EjemploPython/Ejemplo03.py
@@ -0,0 +1,5 @@
+#Sistema para Tipo de Cambio
+montoSoles=float(input("Ingrese el Monto en Soles:"))
+tipoCambio=float(input("Ingrese el tipo de Cambio:"))
+dolar=montoSoles/tipoCambio
+print("La cantidad de Dolar que se obtiene es:",dolar)
---
+++ EjemploPython/Ejemplo04.py
@@ -0,0 +1,6 @@
+#Calcular Promedio de Notas con Ponderacion
+N1=float(input("Nota 01:"))
+N2=float(input("Nota 02:"))
+N3=float(input("Nota 03:"))
+S=N1*0.25+N2*0.25+N3*0.50
+print("La nota del estudiantes es:", S)
