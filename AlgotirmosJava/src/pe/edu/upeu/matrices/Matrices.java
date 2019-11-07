d0e4ec4611a9442f269c8066327212cd5c5c1e49
davidmp <mamanipari@gmail.com>
2019-11-07 08:18:56 -0500

    Matrices

---
+++ AlgotirmosJava/src/pe/edu/upeu/matrices/Matrices.java
@@ -0,0 +1,16 @@
+
+package pe.edu.upeu.matrices;
+
+public class Matrices {
+    public static void main(String[] args) {
+        int[][] M={{2,2,2,2},
+                   {4,4,4,4}, 
+                   {5,5,5,5}, 
+                   {7,7,7,7}, 
+                   {6,6,6,6}, 
+                   {3,3,3,3}, 
+                  };
+        
+        
+    }
+}
